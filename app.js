11bd7718b9f503b24d4a6d23436219f55a911dbd
Фалько Елена Владимировна <67963130+Yellena-dot@users.noreply.github.com>
2022-11-29 00:37:15 +0200

    стоимость работ

--- app.js
+++ app.js
@@ -1,1 +1,6 @@
-console.log("Hi Helena")
+const payRateUSD = 35;
+const projectHours = 40;
+const availableHours = (14 - 4) * 5;
+
+console.log('Смогу ли я работать? '  + (availableHours > projectHours));
+console.log('Стоимость работ ' + (projectHours * payRateUSD) + '$');
